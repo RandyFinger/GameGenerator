ea239fd3f8798dcc7777240464c08bee73cec64d
Randy Finger <randy93finger@gmail.com>
2020-12-31 14:01:24 -0500

    Use pandas

--- GameGenerator.py
+++ GameGenerator.py
@@ -1,16 +1,8 @@
 # Game idea generator
-import csv
+# Must have pandas installed
+import pandas as pd
 import random
 
 # Read csv
-with open('GameIdeas.csv', newline='') as f:
-    reader = csv.reader(f)
-    
-#Print output    
-    for row in reader:
-        print(row[0] + ':')
-        print(row[1] + ':')
-        print(row[2] + ':')
-        print(row[3] + ':')
-        print(row[4] + ':')
-        break
+file = pd.read_csv('GameIdeas.csv')
+print(file)
