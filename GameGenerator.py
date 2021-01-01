2131b643f9656e7bf2df236ebfa68c8223dfb296
Randy Finger <randy93finger@gmail.com>
2021-01-01 10:34:40 -0500

    print output / still need to work on random nan values.

--- GameGenerator.py
+++ GameGenerator.py
@@ -7,15 +7,15 @@ import random
 df = pd.read_csv('GameIdeas.csv')
 
 # Make random selection
-environment = random.randint(1, len(df["Environment"]))
-goal = random.randint(1, len(df["Goal"]))
-genre = random.randint(1, len(df["Genre"]))
-rules = random.randint(1, len(df["Rules"]))
-features = random.randint(1, len(df["Extra Features"]))
+environment = random.randint(0, len(df["Environment"]) + 1)
+goal = random.randint(0, len(df["Goal"]) + 1)
+genre = random.randint(0, len(df["Genre"]) + 1)
+rules = random.randint(0, len(df["Rules"]) + 1)
+features = random.randint(0, len(df["Extra Features"]) + 1)
 
 # Print results
-# print("Environment: " + df["Environment"].)
-# print("Goal: ")
-# print("Genre: ")
-# print("Rules: ")
-# print("Extra Features: ")    
+print("Environment: " + str(df.iat[environment,0]))
+print("Goal: " + str(df.iat[goal,1]))
+print("Genre: " + str(df.iat[genre,2]))
+print("Rules: " + str(df.iat[rules,3]))
+print("Extra Features: " + str(df.iat[features,4]))    
